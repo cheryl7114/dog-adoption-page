eadd0a5a44aeb83ad2230399122687f1718e2c2c
cheryl7114 <157477132+cheryl7114@users.noreply.github.com>
2025-05-04 00:14:11 +0100

    adoption routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\DogController;
+use App\Http\Controllers\AdoptionRequestController;
 
 // Home route
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -10,5 +11,8 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 // Dog routes
 Route::get('/dogs', [DogController::class, 'index']);
 Route::get('/dogs/{id}', [DogController::class, 'show']);
+
+Route::get('/adoption/{dog}', [AdoptionRequestController::class, 'create'])->name('adoption.create');
+Route::post('/adoption/{dog}', [AdoptionRequestController::class, 'store'])->name('adoption.store');
 
 require __DIR__.'/auth.php';
